20d694754aeb2f38fbea4fd26ea290fb5ca6286c
Amr Abdulrahman <abu.muslim.amr@gmail.com>
2017-01-02 09:27:35 +0100

    Improve say module by providing an ignored phrase list

--- say/index.js
+++ say/index.js
@@ -1,15 +1,41 @@
 let say = require('say');
 let q = require('q');
 let _ = require('lodash');
+let fs = require('fs');
 
-module.exports = (string, format) => {
+// read ignored list
+let ignoreListFile = fs.readFileSync(__dirname + '/ignore.txt').toString();
+let ignoreList = ignoreListFile
+  .split('\n\n')
+  .map((term) => term.replace(new RegExp('\n', 'ig'), ''));
+
+module.exports = (text, options) => {
   let defer = q.defer();
 
-  // pretty console log
-  console.log(_.get(string, format, string));
+  options = options || {};
 
-  // speak then resolve the promise
-  say.speak(string, null, 1.1, defer.resolve);
+  // pretty console log
+  console.log(_.get(text, options.format, text));
+
+  if (options.silent) {
+    defer.resolve();
+    return defer.promise;
+  }
+
+  // process ignored list
+  _.each(ignoreList, (ignoreTerm) => {
+    // found a match, remove it
+    if (text.indexOf(ignoreTerm) !== -1) {
+      text = text.replace(ignoreTerm, '');
+    }
+  });
+
+  if (text) {
+    // speak then resolve the promise
+    say.speak(text, null, 1.1, defer.resolve);
+  } else {
+    defer.resolve(); // nothing to say anyway
+  }
 
   return defer.promise;
 };
