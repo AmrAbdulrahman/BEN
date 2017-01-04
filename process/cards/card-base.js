020c6554b798329f43ffae7eb668d5bb07ea67f0
Amr Abdulrahman <abu.muslim.amr@gmail.com>
2017-01-04 23:44:03 +0100

    Fix wrong indentation, convert tabs to spaces

--- process/cards/card-base.js
+++ process/cards/card-base.js
@@ -4,17 +4,17 @@ class CardBase {
 
   getNextEventTime(config) {
     let currentTime = (new Date()).getTime();
-  	let timeSinceStart = currentTime - config.startAt;
+    let timeSinceStart = currentTime - config.startAt;
     let repeatEvery = config.repeat.every * this.unitInMilliSeconds(config.repeat.unit);
 
-  	//This means that there is an event from the same conf
-  	//will get execute in the current second, but we will not consider it.
-  	if (timeSinceStart % repeatEvery == 0) {
+    //This means that there is an event from the same conf
+    //will get execute in the current second, but we will not consider it.
+    if (timeSinceStart % repeatEvery == 0) {
       // @TODO handle this
     }
 
-  	let eventsCount = Math.floor(timeSinceStart / repeatEvery);
-  	return (eventsCount + 1) * repeatEvery + config.startAt;
+    let eventsCount = Math.floor(timeSinceStart / repeatEvery);
+    return (eventsCount + 1) * repeatEvery + config.startAt;
   }
 
   unitInMilliSeconds(unit) {
